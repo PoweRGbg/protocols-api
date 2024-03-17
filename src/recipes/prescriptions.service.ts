5c956ade3f3413453df7ae53548b0af9a3d7c5cf
Roumen Georgiev <roumen_georgiev@yahoo.com>
2024-03-17 12:44:06 +0200

    wip

--- src/recipes/prescriptions.service.ts
+++ src/recipes/prescriptions.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Prescription } from './prescriptions.model';
+import * as fs from 'fs';
 
 @Injectable()
 export class PrescriptionsService {
     private prescriptions: Prescription[] = [];
+    private readonly filePath = 'prescriptions.json';
+
+    constructor() {}
+
+    onModuleInit() {
+        this.readFromFile();
+    }
 
     create(medicineName: string, validTo: Date, issued?: Date): Prescription {
         const id = this.prescriptions.length + 1;
@@ -14,6 +22,7 @@ export class PrescriptionsService {
             issued
         };
         this.prescriptions.push(prescription);
+        this.writeToFile();
         return prescription;
     }
 
@@ -35,6 +44,23 @@ export class PrescriptionsService {
 
     deletePrescription(id: number): void {
         this.prescriptions = this.prescriptions.filter((prescription) => prescription.id !== Number(id));
-        
+        this.writeToFile();
+    }
+
+    private readFromFile(): void {
+        try {
+            const data = fs.readFileSync(this.filePath, 'utf8');
+            this.prescriptions = JSON.parse(data);
+        } catch (error) {
+            console.error('Error reading from file :', error);
+        }
+    }
+
+    private writeToFile(): void {
+        try {
+            fs.writeFileSync(this.filePath, JSON.stringify(this.prescriptions), 'utf8');
+        } catch (error) {
+            console.error('Error writing to file:', error);
+        }
     }
 }
