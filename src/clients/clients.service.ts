5a510ed5f9c2d253b200ad4cdc6ab54b6e13d00d
Roumen Georgiev <roumen_georgiev@yahoo.com>
2024-09-09 15:46:01 +0300

    Don't allow duplicates of ids

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -11,16 +11,40 @@ export class ClientsService {
 
     onModuleInit() {
         this.readFromFile();
+        this.resolveDuplicateIds();
+    }
+
+    private resolveDuplicateIds(): void {
+        const idMap = new Map<number, Client>();
+        let maxId = 0;
+
+        this.clients.forEach(client => {
+            maxId = Math.max(maxId, client.id);
+            if (idMap.has(client.id)) {
+                // Duplicate ID found, assign a new ID
+                client.id = ++maxId;
+            }
+            idMap.set(client.id, client);
+        });
+
+        // Update the clients array with the de-duplicated clients
+        this.clients = Array.from(idMap.values());
+        // Write the updated clients to file
+        this.writeToFile();
     }
 
     create(client: Client, user: string): Client {
-        const id = this.clients.length + 1;
+        const maxId = this.clients.reduce(
+            (max, client) => Math.max(max, client.id),
+            0,
+        );
+        const id = maxId + 1;
         const newClient = { id, ...client, user };
         console.log('client', client);
         console.log('newClient', newClient);
         this.clients.push(newClient);
         this.writeToFile();
-        return client;
+        return newClient;
     }
 
     updateClient(client: Client, user: string): Client {
@@ -67,6 +91,7 @@ export class ClientsService {
         try {
             const data = fs.readFileSync(this.filePath, 'utf8');
             this.clients = JSON.parse(data);
+            this.resolveDuplicateIds(); // Add this line
         } catch (error) {
             if (error.code === 'ENOENT') {
                 this.writeToFile();
